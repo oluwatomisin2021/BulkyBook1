5f165522dc088bb064b794d041f9a91ebe79e6cf
Oluwatomisin Solomon <oluwatomi.ayodele@gmail.com>
2022-04-08 08:39:35 +0100

    Edit , Delete, temp data and notifications

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -29,17 +29,80 @@ public class CategoryController : Controller
     {
         if (obj.Name == obj.DisplayOrder.ToString())
         {
-            ModelState.AddModelError("CustomError","Order name and Display order must not match");
+            ModelState.AddModelError("displayorder","Order name and Display order must not match");
         }
         if (ModelState.IsValid)
         {
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category Created";
             return RedirectToAction("Index");
         }
 
         return View(obj);
         
     }
+
+    //GET
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var categoryFromDb = _db.Categories.Find(id);
+
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+
+        return View(categoryFromDb);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category category)
+    {
+        if(category.Name == category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("displayorder","Display order must not match order name");
+        }
+
+        if(ModelState.IsValid)
+        {
+            _db.Categories.Update(category);
+            _db.SaveChanges();
+            TempData["success"] = "Edit Successful";
+            return RedirectToAction("Index");
+        }       
+        return View(category);
+    }
     
+    public IActionResult Delete(int? id)
+    {
+        var objToDelete = _db.Categories.Find(id);
+        if (objToDelete == null)
+        {
+            return NotFound();
+        }
+
+        return View(objToDelete);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePOST(int? id)
+    {
+        var category = _db.Categories.Find(id);
+        if(category == null)
+        {
+            return NotFound();
+        }
+        _db.Categories.Remove(category);
+        _db.SaveChanges();
+        TempData["success"] = "Category Deleted";
+        return RedirectToAction("Index");
+    }
 }
